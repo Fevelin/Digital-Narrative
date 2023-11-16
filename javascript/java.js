6df358e38fabf99ab1013eeeca400dec41b4ce51
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2023-11-16 15:10:24 +0000

    16/11/23

--- javascript/java.js
+++ javascript/java.js
@@ -1,19 +1,77 @@
 // how to position images making an arc in html and css
 
-function arc(x) {
+var points = document.getElementsByClassName('point');
+
+for (var i = 0; i < points.length; i++) {
+  var point = points[i];
+  // Continue with the rest of the code
+}
+
+point.style.left = (x + 200) + 'px';
+point.style.top = (200 - y) + 'px';
+
+
+function ellipse(x) {
+  // (x/2)^2 + y^2 = 100^2
+  var ySquare = 10000 - (x / 2) * (x / 2)
+  ySquare = Math.abs(ySquare)
+  return Math.sqrt(ySquare)
+}
+
+$(".point").each(function(index) {
+  var x = 200 * (2 * index / 10 - 1)
+  var y = ellipse(x)
+  
+  $(this).css('left', (x + 200) + 'px')
+  $(this).css('top', (100 - y) + 'px')
+})
+
+var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var radius = 75;
+  var startAngle = .8 * Math.PI;
+  var endAngle = 2.2 * Math.PI;
+  var counterClockwise = false;
+
+  context.beginPath();
+  context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+  context.lineWidth = 30;
+
+  // line color
+  context.strokeStyle = 'blue';
+  context.stroke();
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var radius = 75;
+  var startAngle = 8 * Math.PI;
+  var endAngle =2.3 * Math.PI;
+  var counterClockwise = false;
+
+  context.beginPath();
+  context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+  context.lineWidth = 30;
+
+  // line color
+  context.strokeStyle = 'red';
+  context.stroke();
+  // https://www.phind.com/search?cache=d7ai4u2bssq0yiw1dui06mo1
+
+  //
+
+  function arc(x) {
     var ySquare = 10000 - (x ) * (x )
     ySquare = Math.abs(ySquare)
     return Math.sqrt(ySquare)
   }
 
   $(".point").each(function(index) {
-    var x = 50 * (2 * index / 10 - 1)
+    var x = 50 * (6 * index / 10 - 1)
     var y = arc(x)
     
     $(this).css('left', (x + 200) + 'px')
     $(this).css('top', (200 - y) + 'px')
   })
-
-  // https://www.phind.com/search?cache=d7ai4u2bssq0yiw1dui06mo1
-
-  //
