62dfc2006f77681b887af194b0628cb2d9175df2
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2023-11-23 15:28:29 +0000

    2002

--- javascript/java.js
+++ javascript/java.js
@@ -18,28 +18,26 @@ const calculatePointsPositions = () => {
   const Total_Height = window.innerHeight;
   const TOTAL_WIDTH = window.innerWidth; // in px
     //get maximum left and maximum top for the image location
-    const IMAGE_WIDTH = 15*TOTAL_WIDTH/100;
-    const Image_height = 40*Total_Height/100;
+    const IMAGE_WIDTH = 7*TOTAL_WIDTH/100;
 const MAX_LEFT = TOTAL_WIDTH - IMAGE_WIDTH;
-const MAX_TOP =  Total_Height - Image_height;
 
   // manually position images
-  points[0].style.left = 0 + "px";
+  points[0].style.left = 20*TOTAL_WIDTH/100 + "px";
   points[0].style.top = 60*Total_Height/100 + "px";
 
-  points[1].style.left = 1 / 5 * MAX_LEFT + "px";
-  points[1].style.top = 35*Total_Height/100 + "px";
+  points[1].style.left = 30 * TOTAL_WIDTH/100 + "px";
+  points[1].style.top = 50*Total_Height/100 + "px";
 
-  points[2].style.left = 2 / 5 * MAX_LEFT + "px";
-  points[2].style.top = 10*Total_Height/100 + "px";
+  points[2].style.left = 40 * TOTAL_WIDTH/100 + "px";
+  points[2].style.top = 20*Total_Height/100 + "px";
 
-  points[3].style.left = 3 / 5 * MAX_LEFT + "px";
-  points[3].style.top = 10*Total_Height/100 + "px";
+  points[3].style.left = 50 * TOTAL_WIDTH/100 + "px";
+  points[3].style.top = 20*Total_Height/100 + "px";
 
-  points[4].style.left = 4 / 5 * MAX_LEFT + "px";
-  points[4].style.top = 35*Total_Height/100 + "px";
+  points[4].style.left = 60 * TOTAL_WIDTH/100 + "px";
+  points[4].style.top = 50*Total_Height/100 + "px";
 
-  points[5].style.left = MAX_LEFT + "px";
+  points[5].style.left = 70 * TOTAL_WIDTH/100 + "px";
   points[5].style.top = 60*Total_Height/100 + "px";
 }
 
