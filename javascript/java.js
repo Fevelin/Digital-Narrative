6fde77543bdf36207fcde121c0f0cdbdcc06466c
Fevelin <evelin.flamis@student.norwichuni.ac.uk>
2023-11-29 17:20:04 +0000

    202

--- javascript/java.js
+++ javascript/java.js
@@ -65,3 +65,18 @@ function togglePlay() {
 };
 
 playPauseButton.addEventListener('click', togglePlay);
+
+
+// function arc(x) {
+//   var ySquare = 10000 - (x ) * (x )
+//   ySquare = Math.abs(ySquare)
+//   return Math.sqrt(ySquare)
+//  }
+ 
+//  $(".point").each(function(index) {
+//   var x = 50 * (2 * index / 10 - 1)
+//   var y = arc(x)
+  
+//   $(this).css('left', (x + 200) + 'px')
+//   $(this).css('top', (200 - y) + 'px')
+//  })
